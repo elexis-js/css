47a3fe91c2ff81c425fe713af57e80087fc3be5d
defaultkavy <defaultkavy@gmail.com>
2025-04-14 00:54:13 +0800

    v2.0.2 - fix: css id classname error with starting number

--- src/structure/$CSSBaseRule.ts
+++ src/structure/$CSSBaseRule.ts
@@ -1,7 +1,6 @@
 import { $CSSProperty } from "./$CSSProperty";
 import { $CSSStyleSheet } from "./$CSSStyleSheet";
 const LETTER_STR = 'abcdefghijklmnopqrstuvwxyz';
-const NUMBER_STR = '0123456789';
 
 export abstract class $CSSBaseRule {
     properties: $CSSProperty[] = [];
@@ -18,7 +17,7 @@ export abstract class $CSSBaseRule {
     protected static idSet = new Set<string>();
     protected static generateId(length = 5): string { 
         const id = Array.from({length}, (_, i) => { 
-            const char = i === 0 ? LETTER_STR : LETTER_STR + NUMBER_STR;
+            const char = LETTER_STR + LETTER_STR.toUpperCase();
             const rand = Math.round(Math.random() * char.length); return char[rand] 
         }).join(''); 
         if (this.idSet.has(id)) return this.generateId(length);
